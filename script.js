7419696cb1e7e88dc65ede1412fa60db9a340a42
dul <eadndp@gmail.com>
2024-07-04 10:30:36 +0530

    search form

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+let searchForms = document.querySelector('search-form')
+document.querySelector('#search-btn').onclick = () =>{
+    searchForms.classList.toggle('active');
+}   
+
 let cartItems = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
