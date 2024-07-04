07aa4040acfc4804fc8cf6955d64a04383865314
dul <eadndp@gmail.com>
2024-07-04 10:05:58 +0530

    n

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
+
+let cartItems = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItems.classList.toggle('active');
+}
